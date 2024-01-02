c12e7c34a619e0971aecdbc59b247f6e5e7e77ec
chasingboy <39737245+chasingboy@users.noreply.github.com>
2024-01-02 18:03:09 +0800

    Create format-nmap-open-port.py

---
+++ lib/format-nmap-open-port.py
@@ -0,0 +1,57 @@
+# -*- coding=utf-8 -*-
+# python3
+
+import sys
+
+try:
+    from bs4 import BeautifulSoup
+except:
+    print('[ERR] could not import bs4, please install bs4 [python3 -m pip install bs4]')
+    exit()
+
+waf_hosts = ['\n\n# WAF host:']
+
+
+def format_nmap_ports_from_xml(xml):
+    results = ''
+    for host in xml.find_all('host'):
+        address, ports = host.address['addr'], host.ports
+
+        open_ports = ports.find_all('port')
+        if len(open_ports) > 100:
+            waf_hosts.append(address)
+            continue
+        
+        for info in open_ports:
+            if info.state['state'] != 'open': continue
+            
+            port = info['portid']
+            try:
+                service = info.service['name']
+            except:
+                service = 'unknow'
+            
+            #print(address + ':' + port)
+            results += address + ':' + port + '\n'
+    return results
+            
+
+if __name__ == '__main__':
+    filename = sys.argv[1]
+
+    try:
+        with open(filename) as fr: xml = fr.read()
+        xml = BeautifulSoup(xml,'xml')
+    except:
+        #print(f'[ERR] could not open file {filename}')
+        with open(filename,'w') as fw: fw.write(f'[ERR] could not open xml file')
+        exit()
+    
+    results = format_nmap_ports_from_xml(xml)
+    if len(waf_hosts) > 1:
+        results += '\n'.join(waf_hosts)
+    try:
+        with open(filename,'w') as fw: fw.write(results)
+    except:
+        with open(filename,'w') as fw: fw.write('[ERR] script running failed!')
+
