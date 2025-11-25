223aee825ec79ae3119b3ae165a166c90e0d438e
chasingboy <39737245+chasingboy@users.noreply.github.com>
2025-11-25 16:33:26 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -344,7 +344,7 @@ regex_str = r"""
 
     ([a-zA-Z0-9_\-/]{1,}/               # Relative endpoint with /
     [a-zA-Z0-9_\-/]{1,}                 # Resource name
-    \.(?:[a-zA-Z]{1,4}|action)          # Rest + extension (length 1-4 or action)
+    \.(?:[a-zA-Z]{1,10}|action)          # Rest + extension (length 1-10 or action)
     (?:[\?|#][^"|']{0,}|))              # ? or # mark with parameters
 
     |
